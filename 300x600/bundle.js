6b5ee898dc8390fe89942312ea7233f883c2a408
Anniezzyzx <46903927+Anniezzyzx@users.noreply.github.com>
2022-03-08 13:40:43 -0300

    animated banners for Quaker Oats Campaign

---
+++ 300x600/bundle.js
@@ -0,0 +1,156 @@
+(function () {
+  'use strict';
+
+  // BannerUtils version 3.2.0
+  function getBrowser() {
+    // desktop browsers as of 2019-10-04
+    var browserslist = ['other', 'blink', 'chrome', 'safari', 'opera', 'ie', 'edge', 'firefox'];
+    var browser = 0;
+
+    if ('WebkitAppearance' in document.documentElement.style) {
+      browser = 1; // chrome/safari/opera/edge/firefox
+
+      if (/google/i.test(window.navigator.vendor)) browser = 2;
+      if (/apple/i.test(window.navigator.vendor)) browser = 3;
+      if (!!window.opr && !!window.opr.addons || !!window.opera || / OPR\//.test(window.navigator.userAgent)) browser = 4;
+    }
+
+    if (
+    /*@cc_on!@*/
+     !!document.documentMode) browser = 5; // ie 6-11
+
+    if (browser !== 5 && !!window.StyleMedia) browser = 6;
+    if (typeof InstallTrigger !== 'undefined' || 'MozAppearance' in document.documentElement.style) browser = 7;
+    return browserslist[browser];
+  }
+  var browser = getBrowser();
+  function es5() {
+    return parseInt('010', 10) === 10 && function () {
+      return !this;
+    }() && !!(Date && Date.prototype && Date.prototype.toISOString); // IE10, FF21, CH23, SF6, OP15, iOS7, AN4.4
+  }
+  var log = {
+    // https://bit.ly/32ZIpgo
+    traceOn: window.console.log.bind(window.console, '%s'),
+    traceOff: function traceOff() {},
+    trace: window.console.log.bind(window.console, '%s'),
+
+    set debug(bool) {
+      this._debug = bool;
+      bool ? this.trace = this.traceOn : this.trace = this.traceOff;
+    },
+
+    get debug() {
+      return this._debug;
+    }
+
+  };
+  function domIds(scope) {
+    if (scope === void 0) {
+      scope = document;
+    }
+
+    var all = scope.getElementsByTagName('*');
+    var haveIds = {};
+    var i = all.length;
+
+    while (i--) {
+      if (all[i].id) {
+        var safeId = all[i].id.replace(/-|:|\./g, '_');
+        haveIds[safeId] = all[i];
+      }
+    }
+
+    return haveIds;
+  }
+
+  var Banner = {
+    init: function init() {
+      log.debug = true; // set to false before publishing
+
+      var dom = domIds(),
+          ease = Cubic.easeOut; ////////////////////////////////////////////////////// ANIMATION //////////////////////////////////////////////////////
+
+      function frameStart() {
+        if (es5()) {
+          frame0();
+        } else {
+          dom.backup.classList.add('backup');
+        }
+      }
+
+      function animatePack() {
+        dom.animation.classList.remove('invisible');
+        var tl = new TimelineMax();
+        tl.add("start").from("#shadow", 1, {
+          autoAlpha: 0,
+          x: "-=35",
+          ease: ease
+        }, "start").from("#fruits", 1, {
+          scale: 0,
+          ease: ease
+        }, "start").from("#pack", 1, {
+          scale: 0,
+          rotation: -90,
+          ease: ease
+        }, "start").from("#oats,#front-oat", 1, {
+          scale: 0,
+          ease: ease,
+          force3D: false
+        }, "start") // .from("#front-oat",1,{autoAlpha:0, scale:0.5,ease:ease,force3D:false}, "start")
+        .from("#leaf", 1, {
+          scale: 0,
+          ease: ease
+        }, "-=0.8").to("#oats,#front-oat", 4, {
+          scale: 1,
+          force3D: false
+        }, "-=1");
+      }
+
+      function frame0() {
+        var tl = new TimelineMax({
+          onComplete: addRollover
+        });
+        tl.from("#txt1", 1.5, {
+          autoAlpha: 0,
+          x: "-=300",
+          ease: ease
+        }).call(animatePack).from("#txt2", 1.5, {
+          autoAlpha: 0,
+          x: "-=300",
+          ease: ease
+        }, "+=1").from("#txt3", 1.5, {
+          autoAlpha: 0,
+          ease: ease
+        }, "-=1.5").from("#cta", 1, {
+          autoAlpha: 0,
+          ease: ease
+        });
+        dom.ad_content.classList.remove('invisible');
+      } ////////////////////////////////////////////////////// EVENT HANDLERS //////////////////////////////////////////////////////
+
+
+      function addRollover() {
+        dom.ad_content.addEventListener('mouseenter', function () {// Remove this comment
+        });
+        dom.ad_content.addEventListener('mouseleave', function () {// Remove this comment
+        });
+      }
+
+      function adClickThru() {
+        dom.ad_content.addEventListener('click', function () {
+          window.open(window.clickTag || window.clickTAG);
+        });
+      } ////////////////////////////////////////////////////// INIT //////////////////////////////////////////////////////
+
+
+      adClickThru();
+      frameStart();
+    }
+  };
+
+  window.onload = function () {
+    window.requestAnimationFrame(Banner.init);
+  };
+
+}());
